5e84465c890cb903652023a3407cfc5d74438536
Austin Baccus <austinbaccus@live.com>
2019-12-15 16:23:32 -0800

    Test.

--- main.py
+++ main.py
@@ -8,36 +8,41 @@ def get_data_packet():
 
     # TODO
     # poll data sensors and update the values for each variable...
-    lp1 = 0.00
-    lp2 = 0.00
-    lp3 = 0.00
-    lp4 = 0.00
-    g = 0
-    rpm = 0
-    br1 = 0.00
-    br2 = 0.00
-    steering = 0.00
-    throttle = 0.00
-    t = time.time()
+    # lp1 = 0.00
+    # lp2 = 0.00
+    # lp3 = 0.00
+    # lp4 = 0.00
+    # g = 0
+    # rpm = 0
+    # br1 = 0.00
+    # br2 = 0.00
+    # steering = 0.00
+    # throttle = 0.00
+    #t = time.time()
 
     # the fastest string concatenation approach in python (when joining fairly small [n<~100,000] numbers of variables) is to simply use the + operator, as per Jsaon Baker's answer at:
     # https://stackoverflow.com/questions/1316887/what-is-the-most-efficient-string-concatenation-method-in-python#1316959
 
-    return lp1 + '_' + lp2 + '_' + lp3 + '_' + lp4 + '_' + g + '_' + rpm + '_' + br1 + '_' + br2 + '_' + steering + '_' + throttle + '_' + t
+    #return str(lp1) + '_' + str(lp2) + '_' + str(lp3) + '_' + str(lp4) + '_' + str(g) + '_' + str(rpm) + '_' + str(br1) + '_' + str(br2) + '_' + str(steering) + '_' + str(throttle) + '_' + str(t)
+    return str(time.time())
 
 def main():
     # create mqtt instance
-    client = mqtt.Client("wsu_fsae_client")
+    client = mqtt.Client("wsu_fsae_client_1")
 
     # connect to an mqtt broker
-    client.connect("iot.eclipse.org")
+    print("connecting to broker")
+    broker_address="mqtt.eclipse.org"
+    client.connect(broker_address)
+    print("connected")
 
     # while car is on...
     while True:
         # wait a 'lil bit, we don't want to overload the free broker
-        time.sleep(.100)
+        time.sleep(1.000)
 
         # publish the car's telemetry data packet to mqtt broker
+        print("publishing")
         client.publish("wsu/fsae",get_data_packet())
 
 main()
