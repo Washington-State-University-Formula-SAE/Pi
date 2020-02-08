a2e53193416073fbc5a438cdb7d6f2c5f4ac03da
Adam Possing <atom@possing.com>
2020-02-08 13:19:15 -0800

    discovery of tty files works

---
+++ Arduino.py
@@ -0,0 +1,5 @@
+
+class Arduino:
+    def __init__(self, port_id, type):
+        self.portId = portId
+        self.type = type
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import paho.mqtt.client as mqtt
 import time
+import os
+
+
+class MainRunner:
+    def get_tty_files(self, path):
+        tty_files = []
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if file.startswith("tty"):
+                    tty_files.append(os.path.join(root, file))
+        return tty_files
+
+    def identifyArduinos(self):
+        tty_files = self.get_tty_files("/dev/")
+        print(tty_files)
+
+    def __init__(self):
+        self.arduinos = []
+        self.identifyArduinos()
+
 
 def get_data_packet():
     # data packet format:
@@ -26,23 +46,23 @@ def get_data_packet():
     #return str(lp1) + '_' + str(lp2) + '_' + str(lp3) + '_' + str(lp4) + '_' + str(g) + '_' + str(rpm) + '_' + str(br1) + '_' + str(br2) + '_' + str(steering) + '_' + str(throttle) + '_' + str(t)
     return str(time.time())
 
-def main():
+
+if __name__ == '__main__':
+    mainRunner = MainRunner()
     # create mqtt instance
-    client = mqtt.Client("wsu_fsae_client_1")
+    #client = mqtt.Client("wsu_fsae_client_1")
 
     # connect to an mqtt broker
-    print("connecting to broker")
-    broker_address="mqtt.eclipse.org"
-    client.connect(broker_address)
-    print("connected")
+    #print("connecting to broker")
+    #broker_address="mqtt.eclipse.org"
+    #client.connect(broker_address)
+    #print("connected")
 
     # while car is on...
-    while True:
+    #while True:
         # wait a 'lil bit, we don't want to overload the free broker
-        time.sleep(1.000)
+        #time.sleep(1.000)
 
         # publish the car's telemetry data packet to mqtt broker
-        print("publishing")
-        client.publish("wsu/fsae",get_data_packet())
-
-main()
+        #print("publishing")
+        #client.publish("wsu/fsae",get_data_packet())
